ca37225496b6578ba5a3555fca1b3ca98e2a0051
JulienF-OC <jucamlau16@gmail.com>
2025-12-15 03:48:12 +0100

    Refonte Content

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -4,8 +4,15 @@ import "./Content.scss";
 function Content() {
   return (
     <section id="Main">
-      <div className="title">
-        <h1 className="neon-outline-flicker">Développeur web</h1>
+      <div className="hero">
+        <h1>Développeur web</h1>
+
+        <p>
+          Je crée des expériences web modernes, élégantes et efficaces. Chaque
+          projet est pensé pour allier design, performance et clarté.
+        </p>
+
+        <button className="cta-neon">En savoir plus sur moi</button>
       </div>
     </section>
   );
